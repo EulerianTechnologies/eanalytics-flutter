0d166e1bf8a828857c714d9fc3376962421a60d9
Edvin CANDON <edvincandon@gmail.com>
2021-04-26 14:09:40 +0200

    feat: ensure singleton Eulerian class

--- lib/eanalytics.dart
+++ lib/eanalytics.dart
@@ -1,7 +1,25 @@
 library eanalytics;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Eulerian {
+  static final Eulerian _instance = Eulerian._internal();
+  static const SDK_VERSION = "0.0.1";
+
+  bool initialized = false;
+
+  factory Eulerian() {
+    return _instance;
+  }
+
+  Eulerian._internal();
+
+  static void init() {
+    Eulerian._instance.initialized = true;
+  }
+
+  static void track() {
+    assert(Eulerian._instance.initialized,
+        'Eulerian Tracker was not initialized. You must call Eulerian.Init()');
+
+    if (!Eulerian._instance.initialized) return;
+  }
 }
--- test/eanalytics_test.dart
+++ test/eanalytics_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:eanalytics/eanalytics.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Eulerian class should always return singleton instance', () {
+    expect(Eulerian(), Eulerian());
+  });
+
+  test('Calling track before init should throw', () {
+    expect(() => Eulerian.track(), throwsAssertionError);
   });
 }
