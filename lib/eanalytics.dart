3678066e32703272162025291da56ffb5361a47d
Edvin CANDON <edvincandon@gmail.com>
2021-05-02 19:27:28 +0200

    feat: domain sanity checks in Eulerian::init

--- lib/eanalytics.dart
+++ lib/eanalytics.dart
@@ -5,6 +5,7 @@ class Eulerian {
   static const SDK_VERSION = "0.0.1";
 
   bool initialized = false;
+  String? domain;
 
   factory Eulerian() {
     return _instance;
@@ -12,13 +13,15 @@ class Eulerian {
 
   Eulerian._internal();
 
-  static void init() {
+  static void init(String domain) {
+    assert(!domain.contains('.eulerian.com'), 'Domain cannot contain ".eulerian.com"');
+    assert(!Uri.parse('https://$domain').isAbsolute, 'Domain is not well formed');
+
     Eulerian._instance.initialized = true;
   }
 
   static void track() {
-    assert(Eulerian._instance.initialized,
-        'Eulerian Tracker was not initialized. You must call Eulerian.Init()');
+    assert(Eulerian._instance.initialized, 'Eulerian Tracker was not initialized. You must call Eulerian.Init()');
 
     if (!Eulerian._instance.initialized) return;
   }
