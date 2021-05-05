ec9f1a7c10e870374fd958079ac8f19a72e0b7a1
Edvin CANDON <edvincandon@gmail.com>
2021-05-05 07:46:42 +0200

    feat: add post request handler creator utility

---
+++ lib/utils/post.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:eanalytics/eanalytics.dart';
+import 'package:eanalytics/utils/time.dart';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+
+typedef Future<void> OnResultCallback(List<Map<String, dynamic>> body);
+typedef Future<void> PostHandler(List<Map<String, dynamic>> body,
+    {OnResultCallback onSuccess, OnResultCallback onFail});
+
+PostHandler createPostHandler(String domain, Logger logger) {
+  return (body, {OnResultCallback? onSuccess, OnResultCallback? onFail}) async {
+    final url = Uri.https(domain, 'collectorjson/-/${getSecondsSinceEpoch()}');
+    try {
+      logger.d('[EAnalytics] - post request on $url');
+
+      final response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(body),
+      );
+
+      if (response.statusCode != 200) throw HttpException('${response.statusCode}');
+      if (onSuccess != null) await onSuccess(body);
+    } catch (e) {
+      Eulerian.logger.e('[EAnalytics] - Error on post request to ${url.toString()} - ${e.toString()}');
+      if (onFail != null) await onFail(body);
+    }
+  };
+}
