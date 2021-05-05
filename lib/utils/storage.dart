49e221bc79613a8876c81b6b3258dd6043adca3b
Edvin CANDON <edvincandon@gmail.com>
2021-05-05 07:47:56 +0200

    fix: reset stored payloads on save

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -3,22 +3,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 const SAVED_EAPROPERTIES_KEY = '__eanalytics__storage__';
 
-Future<List<dynamic>> getStoredProperties({SharedPreferences? store}) async {
+Future<List<Map<String, dynamic>>> getStoredProperties({SharedPreferences? store}) async {
   final storage = store ?? await SharedPreferences.getInstance();
 
   String? payloads = storage.getString(SAVED_EAPROPERTIES_KEY);
-  return payloads != null ? (jsonDecode(payloads) as Iterable).toList() : [];
+  var parsed = (payloads != null) ? jsonDecode(payloads) : [];
+
+  return (parsed is List) ? parsed.cast<Map<String, dynamic>>() : [];
 }
 
-Future<void> clearStorage() async {
-  final storage = await SharedPreferences.getInstance();
+Future<void> clearStorage({SharedPreferences? store}) async {
+  final storage = store ?? await SharedPreferences.getInstance();
   storage.remove(SAVED_EAPROPERTIES_KEY);
 }
 
-Future<void> save(dynamic payload) async {
+Future<void> save(List<Map<String, dynamic>> payloads) async {
   final storage = await SharedPreferences.getInstance();
-  var data = await getStoredProperties(store: storage);
-
-  data.add(payload);
-  storage.setString(SAVED_EAPROPERTIES_KEY, jsonEncode(data));
+  storage.setString(SAVED_EAPROPERTIES_KEY, jsonEncode(payloads));
 }
