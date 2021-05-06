12a25789dc20df4b31dddd1e54cd7bee0eccbf73
Edvin CANDON <edvincandon@gmail.com>
2021-05-06 09:39:42 +0200

    feat: update example app with single/multiple product tracking

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,15 +24,42 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Eularian Analytics Flutter'),
+        title: const Text('Eularian Analytics TRACKERS'),
       ),
-      body: Row(
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
         children: [
           OutlinedButton(
             onPressed: () {
               Eulerian.track([new EAProperty()]);
             },
-            child: Text('Track EAProperty'),
+            child: Text('EAProperty'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              final params = Params();
+              params.addParam('foo', 'bar');
+              params.addParam('baz', [1, 2, 3]);
+
+              final product = Product(ref: 'p1', name: 'Product 1', group: 'group_of_doom', parameters: params);
+              Eulerian.track([new EAProducts()..addProduct(product)]);
+            },
+            child: Text('Single product'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              final params = Params();
+              params.addParam('foo', {'baz': 'bar'});
+
+              final p1 = Product(ref: 'p1', name: 'Product 1', group: 'group_of_doom');
+              final p2 = Product(ref: 'p2', name: 'Product 2', group: 'group_of_doom', parameters: params);
+              Eulerian.track([new EAProducts()..addProduct(p1)..addProduct(p2)]);
+            },
+            child: Text('Multiple products'),
           )
         ],
       ),
