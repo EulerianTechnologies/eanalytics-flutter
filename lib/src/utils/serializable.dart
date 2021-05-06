76c29a1b7a49a8ef44d38b5b63910ab24104bc65
Edvin CANDON <edvincandon@gmail.com>
2021-05-06 09:37:46 +0200

    feat: add Serializable mixin with traversal helper

---
+++ lib/src/utils/serializable.dart
@@ -0,0 +1,26 @@
+import 'package:eanalytics/src/models/keys/EAPropertyKey.dart';
+import 'package:flutter/foundation.dart';
+
+mixin Serializable<K, V> {
+  @protected
+  Map<K, V> payload = {};
+  Map<String, dynamic> toJson() => toJSON(payload);
+}
+
+bool filterEmpty(dynamic _, dynamic value) =>
+    value == null ||
+    (value is String) && value.isEmpty ||
+    (value is List) && value.isEmpty ||
+    (value is Map && value.isEmpty);
+
+Map<String, dynamic> toJSON<F, T extends Map>(T payload) {
+  payload.removeWhere(filterEmpty);
+  return payload.map((key, value) => (MapEntry(key is EAPropertyKey ? key.name : key as String, traverse(value))));
+}
+
+dynamic traverse(dynamic value) {
+  if (value is Serializable) return value.toJson();
+  if (value is List) return value.map((v) => traverse(v)).toList();
+  if (value is Map) return toJSON(value);
+  return value;
+}
