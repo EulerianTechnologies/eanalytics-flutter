d0f82b516b27dfb8854fa51d673bd4a9a3814d1e
Edvin CANDON <edvincandon@gmail.com>
2023-09-09 11:56:06 +0200

    Use `android_id` package to resolve UUID on android platform

--- lib/src/utils/systemInfo.dart
+++ lib/src/utils/systemInfo.dart
@@ -1,28 +1,18 @@
 import 'dart:io';
 
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:android_id/android_id.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:advertising_id/advertising_id.dart'
     if (dart.library.js) 'package:eanalytics/src/utils/stubs/advertising_id.dart';
 
-enum SystemInfoKey {
-  OS,
-  MODEL,
-  UUID,
-  BUNDLE_ID,
-  APP_NAME,
-  APP_VERSION,
-  IOS_ADID,
-  ANDROID_ADID,
-  IOS_IDFV
-}
+enum SystemInfoKey { OS, MODEL, UUID, BUNDLE_ID, APP_NAME, APP_VERSION, IOS_ADID, ANDROID_ADID, IOS_IDFV }
 
 Future<Map<SystemInfoKey, dynamic>> getSystemInfo() async {
-  if (!kIsWeb && !Platform.isAndroid && !Platform.isIOS)
-    throw PlatformException(code: 'Unsupported platform');
+  if (!kIsWeb && !Platform.isAndroid && !Platform.isIOS) throw PlatformException(code: 'Unsupported platform');
   var systemInfo = <SystemInfoKey, dynamic>{};
 
   systemInfo.addAll(parsePackageInfo(await PackageInfo.fromPlatform()));
@@ -31,8 +21,10 @@ Future<Map<SystemInfoKey, dynamic>> getSystemInfo() async {
 
   if (!kIsWeb) {
     if (Platform.isAndroid) {
+      const _androidIdPlugin = AndroidId();
       systemInfo.addAll(parseAndroidInfo(await device.androidInfo));
       systemInfo[SystemInfoKey.ANDROID_ADID] = await getAdvertiserId(false);
+      systemInfo[SystemInfoKey.UUID] = await _androidIdPlugin.getId();
     }
 
     if (Platform.isIOS) {
@@ -48,10 +40,8 @@ Future<Map<SystemInfoKey, dynamic>> getSystemInfo() async {
 
 Map<SystemInfoKey, String?> parseAndroidInfo(AndroidDeviceInfo info) {
   return <SystemInfoKey, String?>{
-    SystemInfoKey.OS:
-        'Android OS ${info.version.release} (SDK ${info.version.sdkInt})',
-    SystemInfoKey.MODEL: '${info.manufacturer} ${info.model}',
-    SystemInfoKey.UUID: info.androidId
+    SystemInfoKey.OS: 'Android OS ${info.version.release} (SDK ${info.version.sdkInt})',
+    SystemInfoKey.MODEL: '${info.manufacturer} ${info.model}'
   };
 }
 
@@ -81,9 +71,7 @@ Map<SystemInfoKey, String?> parsePackageInfo(PackageInfo info) {
 
 Future<String?> getAdvertiserId(bool requestTrackingAuthorization) async {
   try {
-    if (kIsWeb)
-      throw PlatformException(
-          code: "advertiserId not supported on web platform");
+    if (kIsWeb) throw PlatformException(code: "advertiserId not supported on web platform");
     return await AdvertisingId.id(requestTrackingAuthorization);
   } on PlatformException {
     return null;
